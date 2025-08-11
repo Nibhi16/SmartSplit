b2e8b3a23551c88752fea7a2f6ef7107a7679ca0
Nibhi16 <gargnibhi@gmail.com>
2025-08-11 16:47:12 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="pt-16">
+      <h1 className="text-3xl font-bold underline">
+        My new homepage
+      </h1>
+    </div>
+  );
+}
