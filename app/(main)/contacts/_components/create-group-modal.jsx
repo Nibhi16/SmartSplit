1aaba13420b37c9ce267f6429f8ed96f318e9bdc
Nibhi16 <gargnibhi@gmail.com>
2025-11-07 23:04:01 +0530

    made create group window

--- app/(main)/contacts/_components/create-group-modal.jsx
+++ app/(main)/contacts/_components/create-group-modal.jsx
@@ -2,20 +2,33 @@ import React from 'react'
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog"
 import { useForm } from 'react-hook-form';
 import { zodResolver} from "@hookform/resolvers/zod";
+import z, { optional } from 'zod';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+
+const groupSchema = z.object({
+    name: z.string().min(1, "Group name is required"),
+    description: z.string().optional(),
+});
+
 const CreateGroupModal = ({isOpen, onClose, onSuccess}) => {
     const {register,
         handleSubmit,
         formState:{errors, isSubmitting},
         reset,
     }=useForm({
-        resolver:zodRes
+        resolver:zodResolver(groupSchema),
+        defaultValues:{
+        name:"",
+        description:"",
+    },
     });
 
     const handleClose=()=>{
@@ -26,9 +39,30 @@ const CreateGroupModal = ({isOpen, onClose, onSuccess}) => {
         <Dialog open={isOpen} onOpenChange={handleClose}>
             <DialogContent>
                 <DialogHeader>
-                <DialogTitle>Are you absolutely sure?</DialogTitle>
+                <DialogTitle>Create Group Name</DialogTitle>
                 </DialogHeader>
-                <form></form>
+                <form className="space-y-4">
+                    <div className='space-y-2'>
+                        <Label htmlFor="name">Group Name</Label>
+                        <Input id="name" placeholder="Enter group name"
+                        {...register("name")}/>
+                        {errors.name && (
+                            <p className="text-sm text-red-500">{errors.name.message}</p>
+                        )}
+                    </div>
+                     <div className='space-y-2'>
+                        <Label htmlFor="description">Description {optional}</Label>
+                        <Textarea id="description" placeholder="Enter group description"
+                        {...register("description")}/>
+                    </div>
+                    <div className='space-y-2'>
+                        <Label>Members</Label>
+                        <div>
+                            
+                        </div>
+
+                    </div>
+                </form>
                 <DialogFooter>Footer</DialogFooter>
             </DialogContent>
         </Dialog>
