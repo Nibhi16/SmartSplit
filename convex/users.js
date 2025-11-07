d08ddea1d7cf70f60c8f5404a5c93f42d879e57d
Nibhi16 <gargnibhi@gmail.com>
2025-11-08 00:04:52 +0530

    made searchUser query

--- convex/users.js
+++ convex/users.js
@@ -1,4 +1,6 @@
+import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
+import { internal } from "./_generated/api";
 
 // Store the current user in the "users" table if not already there
 export const store = mutation({
@@ -55,5 +57,40 @@ export const getCurrentUser = query({
     }
 
     return user;
+  },
+});
+
+export const searchUsers = query({
+  args:{query:v.string() },
+  handler: async (ctx, args) => {
+    const currentUser = await ctx.runQuery(internal.users.getCurrentUser);
+    if (args.query.length < 2){
+      return [];
+    }
+    const nameResults = await ctx.db
+      .query("users")
+      .withSearchIndex("search_name", (q) => q.search("name", args.query))
+      .collect();
+
+      const emailResults = await ctx.db
+      .query("users")
+      .withSearchIndex("search_email", (q) => q.search("email", args.query))
+      .collect();
+
+      const users = [
+        ...nameResults,
+        ...emailResults.filter(
+          (email) => !nameResults.some((name) =>name._id === email._id)
+        ),
+      ];
+
+      return users
+        .filter((user) => user._id !== currentUser._id)
+        .map((user) => ({
+          id: user._id,
+          name: user.name,
+          email: user.email,
+          imageUrl: user.imageUrl,
+        }));
   },
 });
